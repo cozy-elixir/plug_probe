bc21aeb6093b5f8e7e693d590a4d546c9671ece0
c4710n <c4710n@users.noreply.github.com>
2023-12-24 14:55:51 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PlugProbe.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "A plug for responding to HTTP probe requests."
   @source_url "https://github.com/cozy-elixir/plug_probe"
 
